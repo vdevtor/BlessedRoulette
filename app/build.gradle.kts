4b2789e79c836ada6c623a9aba2551f52ddf6936
vdevtor <vitaoosp@gmail.com>
2026-09-11 00:39:43 -0300

    atualiza code version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.vitorthemyth.blessedroullet"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.1"
 
         testInstrumentationRunner = "com.vitorthemyth.blessedroullet.di.HiltTestRunner"
